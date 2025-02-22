7b505847fe210e5c70f8e3d9ece707aedf6b7153
Patiwat <patiwat-w@users.noreply.github.com>
2025-02-22 15:45:23 +0700

    Update LoginPage styling and branding; enhance logo appearance and update heading text

--- MUSOS/src/pages/LoginPage.tsx
+++ MUSOS/src/pages/LoginPage.tsx
@@ -130,11 +130,17 @@ const LoginPage: React.FC = () => {
               <div style={{ textAlign: 'center' }}>
                 <img
                   src="images/icons/icon-384x384.png"
-                  alt="MSU SOS Logo"
-                  style={{ width: '150px', marginBottom: '20px' }}
+                  alt="MSU Stroke Triage"
+                  style={{
+                    width: '160px',
+                    marginBottom: '20px',
+                    borderRadius: '50%',
+                    border: '5px solid rgb(231, 9, 9)', // Added border color
+                    boxShadow: '0 8px 16px rgba(0, 0, 0, 0.3)', // Enhanced box shadow for more raised effect
+                  }}
                 />
                 <h2 style={{ fontSize: '2em', fontWeight: 'bold', marginBottom: '30px' }}>
-                  Stroke Triage Data Collection
+                  Stroke Triage Data Collection (Beta.)
                 </h2>
 
                 <IonItem lines="none" style={{ marginBottom: '20px' }}>
